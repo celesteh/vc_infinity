5b62b02c68bf1011021e43e2f99de1b9543e7472
Charles Hutchins <celesteh@gmail.com>
2020-08-04 12:47:18 +0100

    The start of a backend asset manager for uploading audio files

---
+++ upload/config.php
@@ -0,0 +1,18 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'vc_infinity');
+define('DB_PASSWORD', 'rjZE3n4ij4AXxKz');
+define('DB_NAME', 'vc_inifinity');
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+?>
