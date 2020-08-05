6f9ffb71ff20001b7f88971cb2a87e71be3c91a8
Charles Hutchins <celesteh@gmail.com>
2020-08-05 01:51:53 +0100

    Fleshing out registration system

--- upload/register.php
+++ upload/register.php
@@ -11,7 +11,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Validate username
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
+        $username_err = _("Please enter a username.");
     } else{
         // Prepare a select statement
         $sql = "SELECT userid FROM users WHERE username = :username";
@@ -26,22 +26,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 if($stmt->rowCount() == 1){
-                    $username_err = "This username is already taken.";
+                    $username_err = _("This username is already taken.");
                 } else{
                     $username = trim($_POST["username"]);
                 }
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo _("Oops! Something went wrong. Please try again later.");
             }
 
             // Close statement
             unset($stmt);
         }
     }
+
+    // Validate real name
+    $realname = trim($_POST["username"]);
+    if(empty($realname)){
+        // They don't need to give one if they don't want to
+        $realname = "";
+    }
  
     // Validate email
     if(empty(trim($_POST["email"]))){
-        $email_err = "Please enter an email address.";
+        $email_err = _("Please enter an email address.");
     } else{
 	
 	$email = trim($_POST["email"]);
@@ -62,13 +69,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Attempt to execute the prepared statement
                 if($stmt->execute()){
                     if($stmt->rowCount() == 1){
-                       $email_err = "There is already an account for this email.";
+                       $email_err = _("There is already an account for this email.");
                     } else{
                         $email = trim($_POST["email"]);
                     }
                 } else{
 		    $email = "";
-                    echo "Oops! Something went wrong. Please try again later.";
+                    echo _("Oops! Something went wrong. Please try again later.");
                 }
 
                 // Close statement
@@ -77,8 +84,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
         else {
             // invalid address
-	    $email = "";
-	    $email_err = "Please enter a valid email address.";
+	        $email = "";
+	        $email_err = _("Please enter a valid email address.");
         }
 
     }
@@ -86,34 +93,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
    
     // Validate password
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
+        $password_err = _("Please enter a password.");     
     } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+        $password_err = _("Password must have atleast 6 characters.");
     } else{
         $password = trim($_POST["password"]);
     }
     
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
+        $confirm_password_err = _("Please confirm password.");     
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
         if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
+            $confirm_password_err = _("Password did not match.");
         }
     }
+
+    // Check captcha
+    $captchaResult = trim($_POST["captchaResult"]);
+	$firstNumber = trim($_POST["firstNumber"]);
+    $secondNumber = trim($_POST["secondNumber"]);
+    $checkTotal = $firstNumber + $secondNumber;
+
+    if ($captchaResult != $checkTotal) {
+        $captcha_err = _("Incorrect. Please try again.");
+    }
+
+    // Organisation
+    $orgcode = trim($_POST[]("orgcode"));
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($captcha_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, u_password, u_email) VALUES (:username, :password, :email)";
+        $sql = "INSERT INTO users (username, u_password, u_email, u_org, u_realname) VALUES (:username, :password, :email, :orgcode, :realname)";
          
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
             $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
+            $stmt->bindParam(":orgcode", $orgcode, PDO::PARAM_STR);
+            $stmt->bindParam(":realname", $realname, PDO::PARAM_STR);
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
@@ -124,7 +146,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Redirect to login page
                 header("location: login.php");
             } else{
-                echo "Something went wrong. Please try again later.";
+                echo _("Something went wrong. Please try again later.");
             }
 
             // Close statement
@@ -134,6 +156,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Close connection
     unset($pdo);
+} else {
+
+    // Create captcha
+    $min_number = 1;
+	$max_number = 15;
+
+	$random_number1 = mt_rand($min_number, $max_number);
+	$random_number2 = mt_rand($min_number, $max_number);
 }
 ?>
  
@@ -155,7 +185,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
                 <label>Email</label>
-                <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
                 <span class="help-block"><?php echo $email_err; ?></span>
             </div>
 
@@ -174,7 +204,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div> <!-- Organisation -->
+            <div class="form-group ">
+                <label>Group</label>
+                <select name="orgcode" id="orgcode">
+                    <?php        
+                       $sql = "SELECT * FROM `organisations` WHERE 1 ";
+                        if($stmt = $pdo->prepare($sql)){
+                            if($stmt->execute()){
+                                if($row = $stmt->fetch()){
+                                    $orgcode = $row["orgcode"];
+                                    $orgname = $row["orgname"];
+
+                                    echo "<option value='" . $orgcode . "'>" . $orgname . "</option>";
+                            }   
+                    }
+                ?>
+                </select>
+            </div> <!-- Captcha -->
+            <div class="form-group <?php echo (!empty($captcha_err)) ? 'has-error' : ''; ?>">
+                <label>Prove you're a human: <?php echo $random_number1 . ' + ' . $random_number2 . ' = '; ?></label>
+                <input name="captchaResult" type="text" />
+                <span class="help-block"><?php echo $captcha_err; ?></span>
             </div>
+            <input name="firstNumber" type="hidden" value="<?php echo $random_number1; ?>" />
+            <input name="secondNumber" type="hidden" value="<?php echo $random_number2; ?>" />  
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
