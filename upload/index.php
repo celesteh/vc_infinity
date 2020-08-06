9254bb75fcfc02ffead5a7091c5ca48fe461ba88
Charles Hutchins <celesteh@gmail.com>
2020-08-06 16:31:50 +0100

    Navigation

--- upload/index.php
+++ upload/index.php
@@ -1,6 +1,7 @@
 <?php
 // Initialize the session
 session_start();
+include_once "config.php";
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
@@ -16,16 +17,37 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <title>Welcome</title>
     <link rel="stylesheet" href="bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
+        body{ font: 14px sans-serif; width:90%; padding: 20px;}
     </style>
 </head>
 <body>
     <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["realname"]); ?></b>. Welcome to our site.</h1>
+        <h1><b><?php echo htmlspecialchars($_SESSION["realname"]); ?></b> @ <?php echo $_SITE["title"]; ?></h1>
     </div>
+    <?php 
+    if ($_SESSION["powerlevel"] < 20){
+        $approval_required = _("Your account must be approved before you can participate.");
+        echo "<p>" . $approval_required . "</p>\n";
+    }
+    ?>
+    <nav>
+        <?php
+        $powerlevel = $_SESSION["powerlevel"];
+        
+        if (powerlevel >= 80){
+            echo '<p><a href="manage-users.php">Manage and approve users.</a></p>\n';
+        }
+        if ($powerlevel >= 40) {
+            echo '<p><a href="edit-audio.php">Edit audio.</a></p>\n';
+        }
+        if ($powerlevel >= 20) {
+            echo '<p><a href="submit.php">Submit audio.<a/p></p>\n';
+        }
+    ?>
     <p>
         <a href="edit-profile.php" class="btn btn-warning">Edit Profile</a>
         <a href="logout.php" class="btn btn-danger">Logout</a>
     </p>
+</nav>
 </body>
 </html>
