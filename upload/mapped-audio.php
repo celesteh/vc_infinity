e5c12b3ecc421b45277f6a293cd756d062fd3e80
Charles Hutchins <celesteh@gmail.com>
2020-12-03 17:54:21 +0000

    what's going on with this object

--- upload/mapped-audio.php
+++ upload/mapped-audio.php
@@ -248,6 +248,7 @@ EOL;
     
 
         var areas = document.getElementsByTagName("area"); 
+        console.log(areas);
         console.log(typeof areas);
         areas.forEach (function (item, index) {
             var coordStr = item.getAttribute("coords");
