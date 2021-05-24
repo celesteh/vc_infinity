f0a7a072351a5df4aec525718ce4d225d76977fb
Charles Hutchins <celesteh@gmail.com>
2021-05-24 17:38:53 +0100

    IS this brackets?

--- upload/import_audio.php
+++ upload/import_audio.php
@@ -119,10 +119,10 @@ if ($handle = opendir($wav_dir)) {
                                         
                                         // check if the edited audio table already includes this ID.
                                         // if present, does it have a compressed and uncompressed version?
-                                        if ($found && (not $rejected)){
-                                            $exists = true; //assume we're NOT going to add it
-                                            $sql = "SELECT compressed_format, audio_filename FROM edited_audio WHERE original_id = :id";
-                                            echo "$sql\n";
+                                        //if ($found && (not $rejected)){
+                                        //    $exists = true; //assume we're NOT going to add it
+                                        //    $sql = "SELECT compressed_format, audio_filename FROM edited_audio WHERE original_id = :id";
+                                        //    echo "$sql\n";
                                             
                                             /*
                                             if($stmt = $pdo->prepare($sql)){
@@ -167,7 +167,7 @@ if ($handle = opendir($wav_dir)) {
                                                 unset($stmt);
                                             }
                                             */
-                                        }
+                                        //}
                                         /*
                                         // if no flac version, and the wav isn't in the table, save the ID to an array and just add the wav
                                         // this really shouldn't happen
