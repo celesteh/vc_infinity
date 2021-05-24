ee5820bb645ce36812aeba234db4144f894be35c
Charles Hutchins <celesteh@gmail.com>
2021-05-24 14:01:11 +0100

    the 8.0 functions still appear to be not working

--- upload/import_audio.php
+++ upload/import_audio.php
@@ -59,11 +59,12 @@ if ($handle = opendir($wav_dir)) {
                             $path = pathinfo($wav_path);
                             $name = (string) $path['filename']; // it must be a string
                             $new_id = -1;
-                            $rejected = str_ends_with($name, 'REJECT'); 
+                            
 
                             echo "<li>$name\n";
                             
-                            
+                            $rejected = str_ends_with($name, 'REJECT'); 
+                            echo "$rejected\n";
                             // check if the file is a reject
                             if (! $rejected) {
                                 echo "not rejected";
