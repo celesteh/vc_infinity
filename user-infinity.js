854bb0bb25a91cbb24e75f46dc9c93c91c007de1
Charles Hutchins <celesteh@gmail.com>
2021-05-31 17:35:25 +0100

    Play random files

--- user-infinity.js
+++ user-infinity.js
@@ -17,3 +17,54 @@ function loadJSON(path, success, error)
     xhr.open("GET", path, true);
     xhr.send();
 }
+
+function valid (testee){
+    var is_valid = false;
+    var is_valid = (typeof testee != 'undefined');
+    if (is_valid){
+        testee = testee.trim();
+        if (testee.length < 1){
+            is_valid = false;
+        }
+    };
+    return is_valid;
+}
+
+function sortAudioByX(audio_json){
+    //[$x, $y, $dir, $wav, $flac, $meta, $tags, $dur, $user]
+    //var keys = Object.keys(users);
+    var sortedflacs = audio_json.sort(function(a, b) {
+        var ax = a[0];
+        var bx = b[0];
+        if (ax < bx) {
+            return -1; //nameA comes first
+        }
+        if (ax > bx) {
+            return 1; // nameB comes first
+        }
+        return 0;  // names must be equal
+    });
+    return sortedflacs;
+}
+
+function getNRandSorted(audio_json, n){
+
+    var arr = [];
+    for(i=0; i<n ; i++){
+        arr.push(audio_json[Math.floor(Math.random() * audio_json.length)]);
+    };
+
+    arr = sortAudioByX(arr);
+    return arr;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function rrand(low, high){
+    var mulval = high-low;
+    var num = Math.random() * mulval;
+    num = num + low;
+    return num;
+}
