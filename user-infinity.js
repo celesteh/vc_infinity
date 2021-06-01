ea93bd2e6440a84bd3d24596081a7fa7e62d7332
Charles Hutchins <celesteh@gmail.com>
2021-06-01 15:49:32 +0100

    Wrapper for howl objects

--- user-infinity.js
+++ user-infinity.js
@@ -71,3 +71,100 @@ function rrand(low, high){
     num = num + low;
     return num;
 }
+
+
+function AudioClip (json_arr){
+    //[$x, $y, $dir, $wav, $flac, $meta, $tags, $dur, $user]
+    this.x = json_arr[0];
+    this.y = json_arr[1];
+    this.tags = json_arr[6];
+    this.scores = json_arr[5];
+    this.artist = json_arr[8];
+    this.pos = 0;
+    this.ampltiude = 0.5;
+    this.rate = 1;
+    this.src = "processed_audio/" + [json_arr[2] + "/" + json_arr[4]];
+    this.dur = -1;
+ 
+    this.arr = json_arr;
+
+    
+    //this.clip = new Howl ({ 
+    //    src: this.src, 
+    //    preload: true,
+    //    volume: this.amplitude,
+    //    loop: false ,
+    //    onload:  function () {
+    //        /*
+    //        queue.push(howl);
+    //        if (! started){
+    //            started = true;
+    //            //var active = queue.shift();
+    //            setTimeout(player, rrand(200, 3000));
+    //        }
+    //        */
+    //        this.dur = clip.duration();
+    //    },
+    //    onend: function() {
+    //        this.unload();
+    //    }
+    //});
+
+    this.pan = function(pos) { 
+        this.pos = pos;
+        clip.stereo(pos);
+    };
+    this.amp = function(volume) {
+        this.amplitude = volume;
+        clip.volume(volume);
+    };
+    this.setRate = function(rate){
+        this.rate = rate;
+        clip.rate(rate);
+    }
+
+    this.xcomparesort = function(a,b){
+        if (a.x < b.x) {
+            return -1; //nameA comes first
+        }
+        if (a.x > b.x) {
+            return 1; // nameB comes first
+        }
+        return 0;  // names must be equal
+    };
+
+    this.xcompare = function(other){
+        return this.xcomparesort(this,other);
+    }
+
+    this.isCopy = function(other){
+        return (this.src == other.src);
+    }
+
+    this.loaded = function() {
+        return (clip.state() == "loaded");
+    }
+
+    this.load() = function() {
+        this.clip = new Howl ({ 
+            src: this.src, 
+            preload: true,
+            volume: this.amplitude,
+            loop: false
+        });
+
+        this.clip.on("load",function(){
+            this.dur = clip.duration();
+        });
+        this.clip.on("end", function(){
+            this.unload();
+        })
+    }
+
+    this.unload() = function(){
+        clip.unload();
+    }
+
+    this.load();
+    
+}
