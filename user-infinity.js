5da44525f2f9d59fcad0efe8811f4b1286d27fd3
Charles Hutchins <celesteh@gmail.com>
2021-06-03 14:14:37 +0100

    maybe the order matters

--- user-infinity.js
+++ user-infinity.js
@@ -146,9 +146,10 @@ class ImgHandler {
 
                     div.appendChild(self.img);
                     self.img.opacity = 0; // double check
+                    self.img.style.position = "absolute";
                     self.img.style.left = div.offsetTop + "px";//div.style.left + 'px';//self.left + 'px';
                     self.img.style.top = div.offsetLeft + "px";//div.style.top + 'px';//self.top + 'px';
-                    self.img.style.position = "absolute";
+                    
                     
                     
                 }
