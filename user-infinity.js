c5ca8b07b835bcf94101baea0d008cee764b59ad
Charles Hutchins <celesteh@gmail.com>
2021-06-03 20:39:06 +0100

    what are we setting for width and height?

--- user-infinity.js
+++ user-infinity.js
@@ -142,8 +142,14 @@ class ImgHandler {
                     self.img.class = "playback-img" ; //"arr-img anchor-img";
                     //self.img.height = div.clientHeight;
                     //self.img.width = div.clientWidth;
-                    self.img.height = self.img.style.getPropertyValue("height");
-                    self.img.width = self.img.style.getPropertyValue("width");
+                    var w, h;
+                    w = parseInt(self.img.style.getPropertyValue("width"));
+                    h = parseInt(self.img.style.getPropertyValue("height"));
+                    self.img.width = w;
+                    self.img.height = h;
+
+                    console.log("w " + w + " h " + h);
+
                     //var x_offset = self.div.offsetLeft;
                     //var y_offset = self.div.offsetTop;
                     
